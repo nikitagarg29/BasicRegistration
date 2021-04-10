66716a55fb76215a67db29796514589deb704f5a
Nikita <nikitagarg2910@gmail.com>
2021-04-10 13:01:23 +0530

    Updated the files

--- BasicRegistration/settings.py
+++ BasicRegistration/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&gw^5j&s(f$&5xwdjks5nn(5uig=b83p^^$=v6^j-=ok&o=udq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
